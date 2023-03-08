8c30f4c942a65ef79ca49e63ea27385e14e846b0
snowcra5h <snowcra5h@icloud.com>
2023-03-08 14:34:30 -0700

    add wallpaper

--- deco/statusbar.lua
+++ deco/statusbar.lua
@@ -4,13 +4,19 @@ local awful = require("awful")
 
 local _M = {}
 
+-- Custom Local Library: Common Functional Decoration
+local deco = {
+  wallpaper = require("deco.wallpaper")
+}
+
 -- Wibar
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
+    set_wallpaper(s)
 
     -- Create a promptbox for each screen
 
-    -- Create an imagebox widget 
+    -- Create an imagebox widget
 
     -- Create a taglist widget
 
