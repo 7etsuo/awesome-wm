fffc930bf933d21cef26f3831b5df8f2ddc63df3
snowcra5h <snowcra5h@icloud.com>
2023-03-09 00:14:08 -0700

    add tasklist, statustbar, also update rc.lua

--- rc.lua
+++ rc.lua
@@ -1,25 +1,13 @@
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-require("awful.autofocus")
--- Widget and layout library
-local wibox = require("wibox")
--- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
-local naughty = require("naughty")
+local hotkeys_popup = require("awful.hotkeys_popup") -- Enable hotkeys help widget for VIM and other apps
 local menubar = require("menubar")
-local hotkeys_popup = require("awful.hotkeys_popup")
--- Enable hotkeys help widget for VIM and other apps
-
--- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
-
-RC = {}
 
 -- Variables
+RC = {}
 RC.vars = require("main.user-variables")
 
 -- Error handling
@@ -55,13 +43,28 @@ RC.tags = main.tags()
 
 -- Menu
 RC.mainmenu = awful.menu({ items = main.menu() })
+
 RC.launcher = awful.widget.launcher(
   { image = beautiful.awesome_icon, menu = RC.mainmenu }
 )
+
+-- Set the terminal for applications
 menubar.utils.terminal = RC.vars.terminal
 
--- mouse and key bindings
+-- Mouse and Key bindings
 RC.globalkeys = binding.globalkeys()
+RC.globalkeys = binding.bindtotags(RC.globalkeys)
+
+
+-- Set root
+root.buttons(binding.globalbuttons())
+root.keys(RC.globalkeys)
+
+-- Keyboardmap indicator and switcher
+mykeyboardlayout = awful.widget.keyboardlayout()
+
+-- Statusbar: Wibar
+require("deco.statusbar")
 
 -- Rules
 awful.rules.rules = main.rules(
@@ -71,86 +74,6 @@ awful.rules.rules = main.rules(
 
 -- Signals
 require("main.signals")
-
--- Set root
-root.buttons(binding.globalbuttons())
-root.keys(RC.globalkeys)
-
--- Keyboardmap indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
-
--- Statusbar: Wibar
-require("deco.statusbar")
-
-local tasklist_buttons = gears.table.join(
-  awful.button({ }, 1, function (c)
-      if c == client.focus then
-        c.minimized = true
-      else
-        c:emit_signal(
-          "request::activate",
-          "tasklist",
-          {raise = true}
-        )
-      end
-  end),
-  awful.button({ }, 3, function()
-      awful.menu.client_list({ theme = { width = 250 } })
-  end),
-  awful.button({ }, 4, function ()
-      awful.client.focus.byidx(1)
-  end),
-  awful.button({ }, 5, function ()
-      awful.client.focus.byidx(-1)
-end))
-
-awful.screen.connect_for_each_screen(function(s)
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                            awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                            awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                            awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                            awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-      screen  = s,
-      filter  = awful.widget.taglist.filter.all,
-      buttons = taglist_buttons
-    }
-
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-      layout = wibox.layout.align.horizontal,
-      { -- Left widgets
-        layout = wibox.layout.fixed.horizontal,
-        mylauncher,
-        s.mytaglist,
-        s.mypromptbox,
-      },
-      s.mytasklist, -- Middle widget
-      { -- Right widgets
-        layout = wibox.layout.fixed.horizontal,
-        mykeyboardlayout,
-        wibox.widget.systray(),
-        mytextclock,
-        s.mylayoutbox,
-      },
-    }
-end)
-
--- Set keys
-root.keys(globalkeys)
 
 -- Autostart Applications
 awful.spawn.with_shell("1password")
